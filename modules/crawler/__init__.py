e2c946da219860dab1d61a9d10917c8d0dd4deef
wududegw <phandoanhoang191205@gmail.com>
2026-08-25 01:04:07 +0800

    Update modules/crawler/__init__.py - Shopee Automation 3 Steps

---
+++ modules/crawler/__init__.py
@@ -0,0 +1,5 @@
+from .shopee_crawler import ShopeeCrawler
+from .product_parser import ProductParser
+from .dispatcher import ProductDispatcher
+
+__all__ = ["ShopeeCrawler", "ProductParser", "ProductDispatcher"]
